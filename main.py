50538c9675a8e436874f07897eb3f7c386644cea
lukasz markiewicz <lukasz.markiewicz@hotmail.com>
2022-07-10 13:57:19 +0200

    Random app, guess random number between 1 - 20

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from random import randint
+
+count = 1
+randomNumber = randint(1, 20)
+print("Random number: ", randomNumber)
+print('Guess random number between 1 - 20 \n')
+
+while count <= 3:
+    number = input('Podaj liczbę\n')
+    count += 1
+    if number == str(randomNumber):
+        print("Wygrałeś!!!")
+        sys.exit()
+    else:
+        print('\nPróbuj dalej')
+        continue
+
+print("!!!Przegrałeś!!!")
